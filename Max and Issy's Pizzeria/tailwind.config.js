cce94787678c0b345c46bb6ea956134cd5dda65c
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-12-06 17:17:48 +0100

    fix configurations

--- Max and Issy's Pizzeria/tailwind.config.js
+++ Max and Issy's Pizzeria/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        merienda: ['Merienda', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
