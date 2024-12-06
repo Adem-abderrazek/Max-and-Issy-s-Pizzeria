7e658c55a0faf693a6824fa4223a8444b87d3262
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-12-06 17:18:06 +0100

    add component to the app

--- Max and Issy's Pizzeria/src/App.jsx
+++ Max and Issy's Pizzeria/src/App.jsx
@@ -1,9 +1,14 @@
 
 import react from 'react'
+import Hero from './Components/Home/Hero';
+import Menu from './Components/Home/Menu/Menu';
+import Footer from './Components/Home/Footer';
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    <div className=" items-center justify-center h-screen bg-gray-100">
+      <Hero />
+      <Menu/>
+      <Footer/>
     </div>
   );
 }
