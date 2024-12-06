d8405e7c8ed1f31fe90febf91709b962c8c7337c
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-12-06 17:32:25 +0100

    updates

--- Max and Issy's Pizzeria/src/App.jsx
+++ Max and Issy's Pizzeria/src/App.jsx
@@ -3,11 +3,17 @@ import react from 'react'
 import Hero from './Components/Home/Hero';
 import Menu from './Components/Home/Menu/Menu';
 import Footer from './Components/Home/Footer';
+import AboutUs from './Components/Home/AboutUs';
+import Reviews from './Components/Home/Reviews';
+import NavBar from './Components/Home/NavBar';
 function App() {
   return (
     <div className=" items-center justify-center h-screen bg-gray-100">
+      <NavBar/>
       <Hero />
       <Menu/>
+      <AboutUs/>
+      <Reviews/>
       <Footer/>
     </div>
   );
